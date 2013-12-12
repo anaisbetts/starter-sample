a8a04b382ae151ec9c00499ea78689f17264555e
Paul Betts <paul@paulbetts.org>
2013-12-12 12:00:23 -0800

    Move this to Core

--- RxUISample-Core/Models/GiveMeAToken.cs
+++ RxUISample-Core/Models/GiveMeAToken.cs
@@ -6,6 +6,7 @@ namespace RxUISample
     {
         public static string DoIt()
         {
+            // This is a dummy token, get a real one from https://github.com/settings/applications
             return "5efb13cb7d8f024b5950a4c16de4b636ccee486d";
         }
     }
